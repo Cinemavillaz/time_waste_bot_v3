c5fb880e602b49383dad83fc83278846c4747a81
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-07-21 11:09:47 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,13 +147,13 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("ꁅꂦ ꓄ꂦ ꈤꍟꊼ꓄ ᖘꍏꁅꍟ -->>", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f" ⭕️ꎭꍏꊼꀤꎭꀎꎭ ᖘꍏꁅꍟꌗ 1/{len_result if len_result < max_pages else max_pages} ⭕️", callback_data="ignore")
         ])
         
         
